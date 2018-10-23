3f1f36a42ec1bb9a55a5a7967c6b7e505c0d95d9
Daniel Ang <dan.justin.ang@gmail.com>
2018-10-22 20:57:15 -0400

    completed respond_to pages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,21 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
+
+    respond_to do |format|
+      format.html
+      format.text
+      format.json do
+        render json: @books.map { |book_obj|
+          {title: book_obj.title,
+           author: book_obj.author }
+          }
+      end
+      format.csv do
+        render plain: @books.generate_csv(@books)
+      end
+    end
+  end
 
 end
